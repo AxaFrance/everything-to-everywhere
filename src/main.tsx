73847fd570f6cb859de77c5e8ff7434647c070b9
Antoine BALIEU <antoine.balieu@outlook.com>
2025-09-05 10:26:33 +0200

    build: use bundler

--- src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { defineCustomElementReact as importedDefineCustomElementReact } from "./defineCustomElementReact";
-import React from "react";
-
-export const ReactCustom = React;
-export const defineCustomElementReact = importedDefineCustomElementReact;
